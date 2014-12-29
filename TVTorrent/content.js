6ee574c5bad4a445f9ff9ee42bd500ed377bc937
Ben Kuper <bkuperberg@gmail.com>
2014-12-29 23:43:26 +0100

    uploaded for kat support

--- TVTorrent/content.js
+++ TVTorrent/content.js
@@ -24,7 +24,6 @@ function mod()
 		
 		var epName = $(this).find("p a").first().text().replace("!","");
 		var tLink = $("<a href='http://thepiratebay.se/search/"+epName+"/0/7/0'>TPB</a>");
-		
 		var epNumber = "";
 		var testdiv = $(this).find("p a").first();
 		for(var i=0;i<10;i++)
@@ -52,13 +51,36 @@ function mod()
 		
 		var tLink = $("<a href='http://thepiratebay.se/search/"+fullEpSearch+"/0/7/0'>TPB</a>");
 		tLink.addClass("tpb-link");
+		var kLink = $("<a href='https://kickass.so/usearch/"+fullEpSearch+"/?field=seeders&sorder=desc'>KAT</a>");
+		kLink.addClass("kat-link");
 		
 		var modDiv = $("<div class='modDiv'></div>");
 		
 		modDiv.append(tLink);
+		modDiv.append(kLink);
+		
 		$(this).prepend(modDiv);
 		
 		var imgURL = chrome.extension.getURL("magnet-icon.png");
+		var katImgURL = chrome.extension.getURL("kat16.png");
+		
+		$.get("https://kickass.so/usearch/"+fullEpSearch+"/?field=seeders&sorder=desc",null,function(data)
+		{
+			console.log("data !");
+			
+			
+			var result = $(data);
+			
+			var mLink = result.find("a.imagnet").first();
+			console.log("Kat magnet :"+mLink);
+			//mLink.find("img").attr("src",katImgURL);
+			//mLink.find("img").attr("src","http://icons.iconarchive.com/icons/icojam/onebit/24/magnet-icon.png");
+			mLink.html("<img src='"+katImgURL+"'>");//.addClass("kat-magnet");
+			//console.log(div);
+			modDiv.append($(mLink));
+			
+			
+		});
 		
 		$.get("http://thepiratebay.se/search/"+fullEpSearch+"/0/7/0'",null,function(data)
 		{
